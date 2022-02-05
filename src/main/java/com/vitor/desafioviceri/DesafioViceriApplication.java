55d0b813e86de606ceabe0adca73109962a22824
Vitor Lucas <vitorluc.2013@gmail.com>
2022-02-05 10:42:07 -0300

    Criação do Projeto

---
+++ src/main/java/com/vitor/desafioviceri/DesafioViceriApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.desafioviceri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioViceriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioViceriApplication.class, args);
+	}
+
+}
